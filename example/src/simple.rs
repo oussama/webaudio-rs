756624c6682ca646c3e88846dc05312824486262
Oussama <md.oussama@gmail.com>
2018-01-24 02:37:54 +0100

    add example in readme

--- example/src/simple.rs
+++ example/src/simple.rs
@@ -9,12 +9,10 @@ pub fn main() {
     let config = AppConfig::new("Test", size);
     let mut app = App::new(config);
 
-
     let file_data = include_bytes!("../assets/nocturne.ogx");
 
     let ctx = AudioContext::new();
     let mut future = ctx.decode_audio_data(file_data);
-
 
     app.run(move |_t:&mut App| {
         if let Ok(ref data) = future.poll() {
@@ -25,9 +23,7 @@ pub fn main() {
                     source.connect(ctx.destination());       // connect the source to the context's destination (the speakers)
                     source.start(0);
                 }
-                _ => {
-                   // js!{ console.log("not ready yet")};
-                }
+                _ => {}
             }
         }
     });
